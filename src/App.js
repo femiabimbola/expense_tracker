de381b589bcc43c3a33694ed72948bca28c254eb
unknown <femiabimbola6@gmail.com>
2021-01-25 15:37:12 +0100

    created the main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Details from './components/Details/Details';
-
+import Main from './components/Main/Main';
 import useStyles from "./styles";
 
 const App = () => {
@@ -14,8 +14,8 @@ const App = () => {
                 <Grid items xs={12} sm={4}>
                      <Details title="Income"/>
                 </Grid> 
-                <Grid items xs={12} sm={4}>
-                     The main
+                <Grid items xs={12} sm={3}>
+                     < Main />
                 </Grid>
                 <Grid items xs={12} sm={4}>
                      <Details title = "Expense"/>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,7 +11,20 @@ const Main = () => {
         <Card className = { classes.root}>
             <CardHeader  title = "Expense Tracker" subheader = " Designed by Femi Abimbola"/>
             <CardContent>
-
+                <Typography align = "center" variant = "h5"> Total balance is ₦1000 </Typography>
+                <Typography variant="subtitle1" styles = {{ lineHeight : '1.5em', marginTop: '20px'}}>
+                    { /* Info Card */}
+                    Try to say soemthing
+                </Typography>
+                <Divider />
+                 {/* form */}
+            </CardContent>
+            <CardContent className={classes.CardContent}> 
+                <Grid container spacing = {2}>
+                    <Grid item xs = {12}>
+                        {/* { List } */}
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     )
