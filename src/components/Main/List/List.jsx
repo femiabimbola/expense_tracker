3cc54fb44ad828323fc49c012a65f5cd1b3fa01f
unknown <femiabimbola6@gmail.com>
2021-01-27 16:19:39 +0100

    working on list

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import { List as MUList, ListItem, ListItemAvatar, ListItemText,  } from '@material-ui/core';
 import {Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core'
 import { Delete, MoneyOff } from "@material-ui/core"
+
+import useStyles from './styles'
  
 const List = () => {
+    const classes = useStyles();
+
+    const transactions = [
+        { id: 1,}
+    ];
+
     return (
-      <div></div>
+        <MUList dense= {false} className={classes.list}>
+            {transactions.map((transaction) => (
+                <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar className={ transaction.type === "Income" ? classes.avatarIncome : classes.avatarExpense}>
+
+                            </Avatar>
+                        </ListItemAvatar>
+                    </ListItem>
+                </Slide>
+            ))}
+        </MUList>
+
     )
 }
 
