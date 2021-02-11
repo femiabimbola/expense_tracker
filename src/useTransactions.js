f8e256cb139a30ec908e01771c8e7729457f6a0e
unknown <femiabimbola6@gmail.com>
2021-02-11 15:27:57 +0100

    added hook

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -7,4 +7,13 @@ const useTransactions = (title) => {
     
     resetCategories();
     const { transactions } = useContext(ExpenseTrackerContext);
+    const transactionsPerType = transactions.filter((t) => t.type === title)
+    const total = transactionsPerType.reduce((accumulator, currentVal) => accumulator += currentVal.amount );
+
+    const categories = title === 'income' ? incomeCategories:expenseCategories;
+    console.log({total, categories, transactionsPerType})
+
+    transactionsPerType.forEach( t => {
+        const category = categories.find(c => c.type === t.category)
+    });
 }
