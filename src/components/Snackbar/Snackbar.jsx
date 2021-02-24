cae4f743915338f29e7b00874ff85b61c7c3017e
unknown <femiabimbola6@gmail.com>
2021-02-24 16:30:32 +0100

    tody own

---
+++ src/components/Snackbar/Snackbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Snackback } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
+
+import useStyles from './styles'
+
+const CustomizedSnackbar = ({ open, setOpen }) => {
+    const classes = useStyles()
+    const handleClose = (event, reason ) => {
+        if( reason === "clickaway") return;
+    }
+
+    return (
+      <div className={classes.root}>
+          <Snackback
+            anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+            open = {true}
+            autoHideDuration={3000}
+            onClose={handleClose}>
+
+              <MuiAlert 
+                onClose={handleClose} severity='success' 
+                elevation={6} variant = "filled" >
+                  Transaction successfully created  
+              </MuiAlert>
+
+          </Snackback>
+      </div>
+    )
+}
+
+export default CustomizedSnackbar;
